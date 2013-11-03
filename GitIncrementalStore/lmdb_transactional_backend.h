b015822b9ed94e01ed97489d2d760022e378e07a
Bryan Woods <woods.bryan@gmail.com>
2013-11-03 01:12:26 -0700

    Build a static library, for easier use.

--- GitIncrementalStore/lmdb_transactional_backend.h
+++ GitIncrementalStore/lmdb_transactional_backend.h
@@ -10,7 +10,6 @@ GIT_BEGIN_DECL
 
 int git_odb_add_transactional_backend(git_repository * repository, const char * path);
 
-
 int git_odb_transaction_begin(git_repository * repository);
 int git_odb_transaction_commit(git_repository * repository);
 int git_odb_transaction_rollback(git_repository * repository);
